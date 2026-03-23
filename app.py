5281f08a190a7e4d697d61220668b845c62ff042
Suraj <suraj04n@gmail.com>
2026-03-23 15:30:54 +0530

    all pages language change update

--- app.py
+++ app.py
@@ -53,6 +53,7 @@ DB_HOST = "localhost"
 DB_PORT = "5432"
 COW_UPLOAD_FOLDER = os.path.join("static", "uploads", "cow")
 VET_UPLOAD_FOLDER = os.path.join("static", "uploads", "vets")
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 os.makedirs(COW_UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(VET_UPLOAD_FOLDER, exist_ok=True)
